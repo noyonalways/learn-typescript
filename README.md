cc23b4c23d6e1fe7bd50105d26e8460c73cc7530
noyonalways <noyonrahman2003@gmail.com>
2024-01-28 15:30:04 +0600

    initialized commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Learn TypeScript
+
+### What is TypeScript
+
+- TypeScript is the superset of JavaScript created by Microsoft
+- Allows static strict typing
+- Extra features - interfaces, enums, tuples, generics
+- Supports modern features (arrow functions, let, const)
+- Based on the .NET harmony specification
+
+### What's wrong with JavaScript
+
+- Not suitable for large applications
+- Lacks string typing
+- Weird inheritance, unfamiliar syntax
+- Only errors during runtime
+- Suffers type coercion
+
+### Benefits of TypeScript
+
+- TypeScript = JavaScript
+- Compiles to JavaScript - runs everywhere
+- Familiar OOP Programming
+- New Features work everywhere
+
+### Installing TypeScript
+
+- Install Node.js in your machine
+- Install TypeScript compiler
+
+```npm
+npm i -g typescript
+```
---
+++ src/script.ts
@@ -0,0 +1,67 @@
+// function multiply(a: number, b: number) {
+// 	return a * b;
+// }
+
+// console.log(multiply(2, 4));
+
+// array
+// let mixed = ["apple", "orange", 3, true];
+// mixed.push("noyon");
+
+// object
+
+// let person = {
+// 	name: "Noyon",
+// 	age: 20,
+// 	isStudent: true,
+// };
+
+// person.name = "Noyon Rahman";
+// person.country = "Bangladesh";
+
+// let names: string[] = [];
+// names.push("Noyon");
+
+// console.log(names);
+
+// union type
+// let a: (string | number)[] = [];
+// a.push("apple", 45);
+
+// object type
+// let student: {
+// 	name: string;
+// 	age: number;
+// 	isStudent: boolean;
+// };
+
+// student = {
+// 	name: "Noyon",
+// 	age: 20,
+// 	isStudent: true,
+// };
+
+// console.log(student);
+
+// dynamic type
+// let a: any;
+
+// a = 5;
+// a = "noyon";
+
+// let a: any[] = [];
+// a.push("apple");
+// a.push(35);
+
+// let b: {
+// 	name: any;
+// 	age: any;
+// };
+
+// b = {
+// 	name: "noyon",
+// 	age: 20,
+
+// };
+
+// How to use functions
